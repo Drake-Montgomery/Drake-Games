7da54effb8a23ff5edf5be2dece3beaf1235abac
Drake-Montgomery <eder_vip@outlook.com>
2022-05-04 11:30:11 -0300

    Primeiro Commit

---
+++ backend/Drake-Games/src/main/java/com/drake/games/DrakeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.drake.games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrakeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrakeGamesApplication.class, args);
+	}
+
+}
---
+++ backend/Drake-Games/src/test/java/com/drake/games/DrakeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drake.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrakeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
